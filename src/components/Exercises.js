65fa9429936caf4ac9bfa05488766bfe352e4698
Filip <filip.trifunovic23@gmail.com>
2022-12-07 21:44:53 +0100

    exercises card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Pagination } from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, bodyPart, setExercises }) => {
   return (
@@ -10,13 +11,13 @@ const Exercises = ({ exercises, bodyPart, setExercises }) => {
         Showing results
       </Typography>
       <Stack
-        directio="row"
+        direction="row"
         sx={{ gap: { lg: "110px", xs: "50px" } }}
         flexWrap="wrap"
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p key={index}>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
     </Box>
