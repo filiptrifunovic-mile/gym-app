8077f62dd724427aa1ba2e6827c8f8ddc45c186a
Filip <filip.trifunovic23@gmail.com>
2022-12-07 17:29:41 +0100

    searchExercices

--- src/components/SearchExercices.js
+++ src/components/SearchExercices.js
@@ -1,7 +1,68 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExercices = () => {
-  return <div>SearchExercices</div>;
+  const [search, setSearch] = useState("");
+
+  async function handleSearch() {
+    if (search) {
+      // const exercisesData = await fetchData();
+    }
+  }
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "30px" },
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
+          placeholder="Search exercises"
+          type="text"
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
+            },
+
+            width: {
+              lg: "800px",
+              xs: "350px",
+            },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#d32f2f",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercices;
